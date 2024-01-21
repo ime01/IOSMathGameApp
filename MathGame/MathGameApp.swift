9f1e3f9379144e2a2c1eb62d5530fbff23cfae60
Imevbore <ketuimevbore@yahoo.com>
2024-01-21 13:12:10 +0000

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Elena for AP on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
