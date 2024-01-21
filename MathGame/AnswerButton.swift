fd4969a97795fad2a133bc00c3543a4d4ff7bab6
Ime01 <ketuimevbore@yahoo.com>
2024-01-21 14:16:52 +0000

    math game implemented

---
+++ MathGame/AnswerButton.swift
@@ -0,0 +1,18 @@
+//
+//  AnswerButton.swift
+//  MathGame
+//
+//  Created by Elena for AP on 21/01/2024.
+//
+
+import SwiftUI
+
+struct AnswerButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnswerButton()
+}
