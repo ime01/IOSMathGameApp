35d89aa54ba6120c90f61dbb1587b05dd0f303c4
Ime01 <ketuimevbore@yahoo.com>
2024-01-21 14:17:25 +0000

    math game implemented

--- MathGame/AnswerButton.swift
+++ MathGame/AnswerButton.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct AnswerButton: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    var number: Int
+    
+    var body: some View{
+        
+        Text("\(number)")
+            .frame(width: 110, height: 110)
+            .font(.system(size: 40, weight: .bold))
+            .foregroundColor(Color.white)
+            .background(Color.blue)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .padding()
     }
+
 }
 
 #Preview {
-    AnswerButton()
+    AnswerButton(number: 100)
 }
--- MathGame/ContentView.swift
+++ MathGame/ContentView.swift
@@ -8,15 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var correctAnswer = 0
+    @State private var choiceArray: [Int] = [0, 1, 2, 3]
+    @State private var firstNumber = 0
+    @State private var secondNumber = 0
+    @State private var difficulty = 100
+    @State private var score = 0
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        
+        VStack{
+            
+            Text("\(firstNumber) + \(secondNumber)")
+                .font(.largeTitle)
+                .bold()
+            
+            
+            HStack{
+                
+                ForEach(0..<2){ index in
+                    
+                    Button{
+                        isAnswerCorrect(answer: choiceArray[index])
+                            generateAnswers()
+                    }label: {
+                        AnswerButton(number: choiceArray[index])
+                    }
+                    
+                }
+            }
+            
+            
+            HStack{
+                
+                ForEach(2..<4){ index in
+                    
+                    Button{
+                        isAnswerCorrect(answer: choiceArray[index])
+                            generateAnswers()
+                    }label: {
+                        AnswerButton(number: choiceArray[index])
+                    }
+                    
+                }
+            }
+            
+            Text("Score: \(score)")
+                .font(.largeTitle)
+                .bold()
+            
+            
+            
+            
+        }.onAppear(perform: generateAnswers)
+       
+            
+       
     }
+    
+    
+    
+    func generateAnswers(){
+        firstNumber = Int.random(in: 0...(difficulty/2))
+        secondNumber = Int.random(in: 0...(difficulty/2))
+        var answerList = [Int]()
+        
+        correctAnswer = firstNumber + secondNumber
+        
+        for i in 0...2{
+            answerList.append(Int.random(in: 0...difficulty))
+        }
+        
+        answerList.append(correctAnswer)
+        
+        choiceArray = answerList.shuffled()
+    }
+    
+    
+    func isAnswerCorrect(answer:Int){
+        let isCorrect = answer == correctAnswer ? true : false
+        
+        if isCorrect{
+            self.score += 1
+        }else{
+            self.score -= 1
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
 
 #Preview {
